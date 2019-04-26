26e5d0bcf2f8c21c97be6148d4698c7e4ff2066c
Harshal <harshalgohel@gmail.com>
2019-04-26 17:45:36 +0530

    Universal Navbar, Updated production entry form, made all tables responsive, fixed sorting, added custom css for future use

--- src/main/resources/public/functionality.js
+++ src/main/resources/public/functionality.js
@@ -49,3 +49,37 @@ myFilter = function(id, sel, filter) {
     }
   }
 };
+numericalSort = function(id, sel, sortvalue) {
+  var a, b, i, ii, y, bytt, v1, v2, cc, j;
+  a = w3.getElements(id);
+  for (i = 0; i < a.length; i++) {
+    for (j = 0; j < 2; j++) {
+      cc = 0;
+      y = 1;
+      while (y == 1) {
+        y = 0;
+        b = a[i].querySelectorAll(sel);
+        for (ii = 0; ii < (b.length - 1); ii++) {
+          bytt = 0;
+          if (sortvalue) {
+            v1 = Number(b[ii].querySelector(sortvalue).innerHTML.toLowerCase());
+            v2 = Number(b[ii + 1].querySelector(sortvalue).innerHTML.toLowerCase());
+          } else {
+            v1 = Number(b[ii].innerHTML.toLowerCase());
+            v2 = Number(b[ii + 1].innerHTML.toLowerCase());
+          }
+          if ((j == 0 && (v1 > v2)) || (j == 1 && (v1 < v2))) {
+            bytt = 1;
+            break;
+          }
+        }
+        if (bytt == 1) {
+          b[ii].parentNode.insertBefore(b[ii + 1], b[ii]);
+          y = 1;
+          cc++;
+        }
+      }
+      if (cc > 0) {break;}
+    }
+  }
+};
